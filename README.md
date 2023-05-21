5d11a66299ba63b08c0742fabc74306acf8a4288
Estee <estee.tsw@gmail.com>
2023-05-21 20:44:06 +0800

    create action

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# deepl-translate-github-action
+# DeepL Translate Github Action 
+
+This action uses the DeepL Translate API to translate text files in your repository to your target languages. The output will follow the file extension of the given input file.
+
+## Inputs
+
+| Name                  | Description                                      | Required | Default | 
+| --------------------- | ------------------------------------------------ |:--------:| ------- |
+| `deepl_api_key`       | API Key for DeepL API                            |   yes     |  |
+| `target_languages`    | Target languages to translate to                 |   yes    |         |
+| `input_file_path`     | Path of the file you want to translate           |   yes    |         |  
+| `output_file_name_prefix` | Prefix of the output file name, including folder name |   no     |         |
+| `ignore_terms`        | Terms to ignore when translating                 |   no     |         |  
+| `no_translate_start_tag` | Start tag to ignore when translating           |   no     |  <!-- notranslate -->  |
+| `no_translate_end_tag`   | End tag to ignore when translating                |   no     | <!-- /notranslate --> |
+
+
+## How to use
+
+<details><summary>How to get DeepL API Token</summary>
+
+First, you need to [sign up for a DeepL account](https://www.deepl.com/). Then you can go to https://www.deepl.com/account/summary and retrieve your token there.
+
+![Screenshot of where the auth key is located on the website](DeepL_API_Auth_Key_Example.png)
+
+</details>
+
+<details><summary>If you don't have an existing GitHub Action workflow for your repository</summary>
+
+1. Create a folder `.github/workflows` if you don't have it already 
+2. Inside that folder, create a YAML file say `translate.yml`
+3. In the `translate.yml` file, you can copy the example below and modify it to your usage.
+</details>
+
+## Example usage
+
+```yaml
+- name: Translate Readme
+  uses: lyqht/deepl-translate-action@v1
+  with:
+    target_languages: 'zh,ja'
+    input_file_path: 'README.md'
+    output_file_name_prefix: 'docs/README.'  
+```
+
+This will translate the README.md file into Chinese and Japanese, and save them as `docs/README.zh.md` and `docs/README.ja.md`.
+
+## Demo
+
+For a demo, refer to this repository [deepl-demo](https://github.com/lyqht/deepl-demo).
---
+++ index.ts
@@ -0,0 +1,84 @@
+import * as deepl from 'deepl-node';
+import fs from 'fs';
+import path from 'path';
+
+const authKey = process.env.deepl_api_key as string;
+const translator = new deepl.Translator(authKey);
+const targetLanguages = process.env.target_languages?.split(',') as string[];
+const inputFilePath = process.env.input_file_path as string;
+const outputFileNamePrefix = process.env.output_file_name_prefix as string;
+const termsToIgnoreForTranslation = process.env.ignore_terms?.split(',') || [] as string[];
+const startTagForNoTranslate = process.env.no_translate_start_tag as string;
+const endTagForNoTranslate = process.env.no_translate_end_tag as string;
+
+const tempFilePath = 'to_translate.txt';
+
+function replaceAll(str: string, search: string, replacement: string): string {
+    let index = str.indexOf(search);
+    while (index != -1) {
+      str = str.replace(search, replacement);
+      index = str.indexOf(search);
+    }
+    return str;
+}
+
+// main
+(async () => {
+    if (termsToIgnoreForTranslation.length > 0)
+    fs.readFile(inputFilePath, 'utf8', async function (err, text) {
+        if (err) {
+            return console.log(err);
+        }
+
+        termsToIgnoreForTranslation.forEach(term => {
+            text = text.replace(new RegExp(term, 'g'), `<keep>${term}</keep>`)
+        })
+
+        const textWithNoTranslateStartTagReplaced = replaceAll(text, startTagForNoTranslate, '<keep>')
+        const textWithNoTranslateEndTagReplaced = replaceAll(textWithNoTranslateStartTagReplaced, endTagForNoTranslate, '</keep>')
+
+
+        fs.writeFile(tempFilePath, textWithNoTranslateEndTagReplaced, function (err) {
+            if (err) return console.log(err);
+            console.log(`Created file to be translated`)
+        })
+    })
+
+    const fileExtension = path.extname(inputFilePath)
+
+    const tempFileExists = fs.existsSync(tempFilePath)
+    const translateFilePath = tempFileExists ? tempFilePath : inputFilePath
+
+    console.log(`DeepL supports ${targetLanguages.length} languages. Translating the input file into all supported languages...`)
+    fs.readFile(translateFilePath, 'utf8', async function (err, text) {
+        if (err) {
+            return console.log(err);
+        }
+        
+        for (const targetLanguage of targetLanguages) {
+            const targetLang = targetLanguage as deepl.TargetLanguageCode
+            const textResult = await translator.translateText(
+                text,
+                null,
+                targetLang,
+                {
+                    preserveFormatting: true,
+                    tagHandling: 'xml',
+                    ignoreTags: ['keep']
+                }
+            );
+
+            const translatedText = textResult.text;
+            
+            // remove added keep tags
+            const textWithNoTranslateStartTagReplaced = replaceAll(translatedText, '<keep>', '')
+            const textWithNoTranslateEndTagReplaced = replaceAll(textWithNoTranslateStartTagReplaced, '</keep>', '')
+            
+    
+            fs.writeFile(`${outputFileNamePrefix}${targetLang}.${fileExtension}`, textWithNoTranslateEndTagReplaced, function (err) {
+                if (err) return console.log(err);
+                console.log(`Translated ${targetLang}`)
+            });
+        }
+    });
+})();
