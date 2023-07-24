5578967645bae696cd989ed50870b805268d2afd
Estee <estee.tsw@gmail.com>
2023-07-24 19:50:34 +0800

    fix(utils.ts): fix translateRecursive function

--- src/utils.ts
+++ src/utils.ts
@@ -10,75 +10,103 @@ function replaceAll(str: string, search: string, replacement: string): string {
 }
 
 function replaceParameterStringsInJSONValueWithKeepTags(value: string): string {
-    const termRegex = /({{.*?}}|{.*?})/g;
-    return value.replace(termRegex, (match) => `<keep>${match}</keep>`);
+	const termRegex = /({{.*?}}|{.*?})/g;
+	return value.replace(termRegex, (match) => `<keep>${match}</keep>`);
 }
 
 function removeKeepTagsFromString(str: string): string {
-	if (!str.includes('<keep>')) return str
+	if (!str.includes("<keep>")) return str;
 
-	const textWithNoTranslateStartTagReplaced = replaceAll(
-		str,
-		"<keep>",
-		"",
-	);
+	const textWithNoTranslateStartTagReplaced = replaceAll(str, "<keep>", "");
 	const textWithNoTranslateEndTagReplaced = replaceAll(
 		textWithNoTranslateStartTagReplaced,
 		"</keep>",
 		"",
 	);
-	return textWithNoTranslateEndTagReplaced
+	return textWithNoTranslateEndTagReplaced;
 }
 
 type PossibleRecursive<T> = {
 	[K in keyof T]: T[K] extends object ? PossibleRecursive<T[K]> : T[K];
-  };
+};
 
-type TranslatedJSONResults = Record<TargetLanguageCode, PossibleRecursive<Record<string, string>>>;
+type TranslatedJSONResults = Record<
+	TargetLanguageCode,
+	PossibleRecursive<Record<string, string>>
+>;
 
-const translateRecursive = async (inputJson: Record<string, any>, targetLanguages: TargetLanguageCode[], translator: Translator, translatedResults: TranslatedJSONResults) => {
+const translateRecursive = async (
+	inputJson: Record<string, any>,
+	targetLanguages: TargetLanguageCode[],
+	translator: Translator,
+	translatedResults: TranslatedJSONResults,
+	path: string[] = [], // new argument to keep track of the object's nested path
+) => {
 	const keys = Object.keys(inputJson);
 
 	for (const key of keys) {
-	  if (typeof inputJson[key] === 'object') {
-		await translateRecursive(inputJson[key], targetLanguages, translator, translatedResults);
-		continue;
-	  }
+		let newPath = [...path, key];
 
-	  const value = inputJson[key];
-	  const textToBeTranslated = replaceParameterStringsInJSONValueWithKeepTags(value)
-
-	  console.info(`Translating the input file into ${targetLanguages.length} languages...`);
-
-	  for (const targetLanguage of targetLanguages) {
-		const textResult = (await translator.translateText(
-		  textToBeTranslated,
-		  null,
-		  targetLanguage,
-		  {
-			preserveFormatting: true,
-			tagHandling: "xml",
-			ignoreTags: ["keep"],
-		  },
-		)) as TextResult;
-
-		if (!translatedResults[targetLanguage]) {
-		  translatedResults[targetLanguage] = {};
+		if (typeof inputJson[key] === "object") {
+			await translateRecursive(
+				inputJson[key],
+				targetLanguages,
+				translator,
+				translatedResults,
+				newPath, // pass the extended path when the current key is an object
+			);
+			continue;
 		}
 
-		const translatedText = textResult.text;
-		const resultText = removeKeepTagsFromString(translatedText);
-		translatedResults[targetLanguage][key] = resultText;
-	  }
+		const value = inputJson[key];
+		const textToBeTranslated =
+			replaceParameterStringsInJSONValueWithKeepTags(value);
+
+		for (const targetLanguage of targetLanguages) {
+			const textResult = (await translator.translateText(
+				textToBeTranslated,
+				null,
+				targetLanguage,
+				{
+					preserveFormatting: true,
+					tagHandling: "xml",
+					ignoreTags: ["keep"],
+				},
+			)) as TextResult;
+
+			if (!translatedResults[targetLanguage]) {
+				translatedResults[targetLanguage] = {};
+			}
+
+			const translatedText = textResult.text;
+			const resultText = removeKeepTagsFromString(translatedText);
+
+			// Assign the translated text to its original position in object
+			newPath.reduce<Record<string, string | Record<string, string>>>(
+				(prev, curr, idx) => {
+					if (idx === newPath.length - 1) {
+						prev[curr] = resultText;
+					} else if (!prev[curr]) {
+						prev[curr] = {};
+					}
+
+					return prev[curr] as Record<string, string | Record<string, string>>;
+				},
+				translatedResults[targetLanguage] as Record<
+					string,
+					string | Record<string, string>
+				>,
+			);
+		}
 	}
 
 	return translatedResults;
-  }
+};
 
-  export {
-    replaceAll,
-    removeKeepTagsFromString,
+export {
+	replaceAll,
+	removeKeepTagsFromString,
 	replaceParameterStringsInJSONValueWithKeepTags,
 	translateRecursive,
-	TranslatedJSONResults
-}
+	TranslatedJSONResults,
+};
